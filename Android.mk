161acee214c5af56498a52dbfbf4f5d3b4bdb530
Hua Fu <hua.fu@mediatek.com>
2019-08-06 22:44:22 +0800

    [ALPS04088869] GPS: Remove GPS data link drivers
    GPS data link drivers are for chips with
    L1 and L5 dual frequency support (such as MT6885).
    
    Due to it no need on this branch, remove it.
    
    MTK-Commit-Id: 8b2cabb7aa6938b8a3642b60018cea87c2d5c4bd
    
    Change-Id: Ie90312405c61f80d1178936aaf30bdf1667acaba
    Signed-off-by: Hua Fu <hua.fu@mediatek.com>
    CR-Id: ALPS04088869
    Feature: Location Chipset Capability

--- Android.mk
+++ Android.mk
@@ -8,17 +8,7 @@ LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_OWNER := mtk
 LOCAL_INIT_RC := init.gps_drv.rc
 
-ifneq (,$(filter MT6885,$(MTK_PLATFORM)))
-#Only set dependency to conninfra.ko when CONSYS_6885 set.
-ifneq (,$(filter CONSYS_6885,$(MTK_COMBO_CHIP)))
-LOCAL_REQUIRED_MODULES := conninfra.ko
-else
-$(warning MTK_PLATFORM=$(MTK_PLATFORM), MTK_COMBO_CHIP=$(MTK_COMBO_CHIP))
-$(warning gps_drv.ko does not claim the requirement for conninfra.ko)
-endif
-else
 LOCAL_REQUIRED_MODULES := wmt_drv.ko
-endif
 
 include $(MTK_KERNEL_MODULE)
 
