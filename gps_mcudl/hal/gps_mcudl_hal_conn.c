e05e4ccaba351a167885e103a57387b2da6edd01
Hua Fu <hua.fu@mediatek.com>
2024-01-12 20:10:34 +0800

    [ALPS07821738] GPS: Vote vcore opp v1+v2(sync but disable voting)
    Vote vcore opp v1+v2.
    This patch is for MT6985, sync it for common code.
    But disable voting due to other chips do not need voting.
    
    MTK-Commit-Id: e279ec303188f868a2a32cdd635dd477e7b7cbdd
    
    Signed-off-by: Hua Fu <hua.fu@mediatek.com>
    Change-Id: I7fdfae52cff60d64b8b031a5501c3fa8faabf93d
    CR-Id: ALPS07821738
    Feature: gnss_host_kernel_driver

--- gps_mcudl/hal/gps_mcudl_hal_conn.c
+++ gps_mcudl/hal/gps_mcudl_hal_conn.c
@@ -8,7 +8,12 @@
 #include "gps_mcudl_log.h"
 #include "gps_mcudl_hal_conn.h"
 #include "gps_mcudl_ylink.h"
+#include "gps_dl_hal.h"
 #include "gps_dl_hw_dep_api.h"
+#include "gps_dl_time_tick.h"
+#include "gps_mcudl_hw_mcu.h"
+#include "gps_mcudl_hal_conn.h"
+#include "gps_mcusys_fsm.h"
 
 
 void gps_mcudl_hal_get_ecid_info(void)
@@ -63,3 +68,149 @@ bool gps_mcudl_hal_dump_power_state(void)
 		L1_on_mode, L5_on_mode, L1_off_mode, L5_off_mode, flag, on_off_cnt, xbitmask);
 	return is_gps_awake;
 }
+
+#if GPS_DL_ON_LINUX
+bool gps_mcudl_is_voting_for_coinninfra_on;
+unsigned long gps_mcudl_vote_for_coinninfra_on_us;
+unsigned long gps_mcudl_vote_phase_bitmask;
+
+static void gps_mcudl_vote_to_deny_opp0(bool deny_opp0)
+{
+    /* Only MT6985 need it (from Android-T)
+     * Enable it when we need to support MT6985 MP(e.g. vendor unfreeze, might be on Android-X).
+     * Condition "!g_gps_common_on || gps_dl_hal_get_conn_infra_ver() == GDL_HW_CONN_INFRA_VER_MT6985"
+     * can be used for runtime dispatching.
+     */
+#if 0
+#include "dvfsrc-common.h"
+	mtk_dvfsrc_dynamic_opp0(VCOREOPP_GPS, deny_opp0);
+#endif
+}
+
+void gps_mcudl_set_opp_vote_phase(enum gps_mcudl_hal_opp_vote_phase phase, bool is_in)
+{
+	smp_mb__before_atomic();
+	if (is_in)
+		set_bit(phase, &gps_mcudl_vote_phase_bitmask);
+	else
+		clear_bit(phase, &gps_mcudl_vote_phase_bitmask);
+	smp_mb__after_atomic();
+}
+
+void gps_mcudl_end_all_opp_vote_phase(void)
+{
+	unsigned long bitmask;
+
+	bitmask = gps_mcudl_vote_phase_bitmask;
+	if (bitmask != 0)
+		GDL_LOGW("bitmask=0x%08lx is not zero", bitmask);
+	gps_mcudl_vote_phase_bitmask = 0;
+}
+
+unsigned int gps_mcudl_get_opp_vote_phase_bitmask(void)
+{
+	unsigned long bitmask;
+
+	bitmask = gps_mcudl_vote_phase_bitmask;
+	return (unsigned int)bitmask;
+}
+
+void gps_mcudl_vote_to_deny_opp0_for_coinninfra_on(bool vote)
+{
+	unsigned long d_us;
+
+	if (vote) {
+		if (gps_mcudl_is_voting_for_coinninfra_on) {
+			d_us = gps_dl_tick_get_us() - gps_mcudl_vote_for_coinninfra_on_us;
+			GDL_LOGE("double vote true, d_us=%lu", d_us);
+			return;
+		}
+		gps_mcudl_is_voting_for_coinninfra_on = true;
+		gps_mcudl_vote_for_coinninfra_on_us = gps_dl_tick_get_us();
+		gps_mcudl_vote_to_deny_opp0(true);
+	} else {
+		if (!gps_mcudl_is_voting_for_coinninfra_on) {
+			GDL_LOGW("double vote false");
+			return;
+		}
+		gps_mcudl_vote_to_deny_opp0(false);
+		d_us = gps_dl_tick_get_us() - gps_mcudl_vote_for_coinninfra_on_us;
+		gps_mcudl_is_voting_for_coinninfra_on = false;
+		GDL_LOGE("abnormal vote end-1, d_us=%lu", d_us);
+	}
+}
+
+bool gps_mcudl_hw_conn_force_wake(bool enable)
+{
+	bool wake_okay = false;
+	bool conn_on_vote = gps_mcudl_is_voting_for_coinninfra_on;
+	unsigned int vote_bitmask;
+	unsigned long us0, d_us;
+	bool conn_ver_valid = false;
+	unsigned int conn_ver = gps_dl_hal_get_conn_infra_ver();
+	enum gps_mcusys_gpsbin_state mcu_state = gps_mcusys_gpsbin_state_get();
+
+	if (!enable) {
+		wake_okay = gps_mcudl_hw_conn_force_wake_inner(false);
+		return wake_okay;
+	}
+
+	vote_bitmask = gps_mcudl_get_opp_vote_phase_bitmask();
+	if (vote_bitmask == 0) {
+		wake_okay = gps_mcudl_hw_conn_force_wake_inner(true);
+		if (conn_on_vote) {
+			gps_mcudl_vote_to_deny_opp0(false);
+			d_us = gps_dl_tick_get_us() - gps_mcudl_vote_for_coinninfra_on_us;
+			gps_mcudl_is_voting_for_coinninfra_on = false;
+			GDL_LOGE("abnormal vote end-2, d_us=%lu, w_ok=%d", d_us, wake_okay);
+		}
+		return wake_okay;
+	}
+
+	if (conn_on_vote) {
+		us0 = gps_mcudl_vote_for_coinninfra_on_us;
+		gps_mcudl_is_voting_for_coinninfra_on = false;
+	} else {
+		us0 = gps_dl_tick_get_us();
+		gps_mcudl_vote_to_deny_opp0(true);
+	}
+	wake_okay = gps_mcudl_hw_conn_force_wake_inner(true);
+	gps_mcudl_vote_to_deny_opp0(false);
+	d_us = gps_dl_tick_get_us() - us0;
+	conn_ver_valid = (mcu_state == GPS_MCUSYS_GPSBIN_POST_ON);
+	if ((d_us >= 2000) || (!conn_ver_valid) ||
+		(conn_ver == GDL_HW_CONN_INFRA_VER_MT6985)) {
+		GDL_LOGW("vote end: d_us=%lu, bitmask=0x%08x, is_cv=%d, w_ok=%d, st=%d,0x%x",
+			d_us, vote_bitmask, conn_on_vote, wake_okay, mcu_state, conn_ver);
+	} else {
+		GDL_LOGD("vote end: d_us=%lu, bitmask=0x%08x, is_cv=%d, w_ok=%d, st=%d,0x%x",
+			d_us, vote_bitmask, conn_on_vote, wake_okay, mcu_state, conn_ver);
+	}
+	return wake_okay;
+}
+
+#else
+void gps_mcudl_set_opp_vote_phase(enum gps_mcudl_hal_opp_vote_phase phase, bool is_in)
+{
+}
+
+void gps_mcudl_end_all_opp_vote_phase(void)
+{
+}
+
+unsigned int gps_mcudl_get_opp_vote_phase_bitmask(void)
+{
+	return 0;
+}
+
+void gps_mcudl_vote_to_deny_opp0_for_coinninfra_on(bool vote)
+{
+}
+
+bool gps_mcudl_hw_conn_force_wake(bool enable)
+{
+	return gps_mcudl_hw_conn_force_wake_inner(enable);
+}
+
+#endif
+
