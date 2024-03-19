f8eea30ae696681795fd7d3cca44c19dab77d9d1
Hua Fu <hua.fu@mediatek.com>
2024-03-19 09:54:47 +0800

    [ALPS08755148] GPS: Improve mbraink vs gnss data struct
    [Description]
    1. Add/rename some data fields
    2. Improve interface compatibility
    
    [Test]
    1. GNSS data can be gotten by mbrain
    2. GNSS function ok
    
    MTK-Commit-Id: 0e272a5434c633ce26f986a6cf712dc8cf1c8f07
    
    Signed-off-by: Hua Fu <hua.fu@mediatek.com>
    CR-Id: ALPS08755148
    Feature: conn_gps_driver
    Change-Id: I19fb4b201f19546ae85430771f40b45ab9b16bab

--- gps_mcudl/linux/gps_mcudl_linux_mbraink.c
+++ gps_mcudl/linux/gps_mcudl_linux_mbraink.c
@@ -23,7 +23,7 @@ void gps_mcudl_linux_register_cbs_to_mbraink(void)
 
 	pfn = symbol_get(register_gps2mbraink_ops);
 	if (pfn == NULL) {
-		GDL_LOGE("symbol_get fail");
+		GDL_LOGW("symbol_get fail");
 		return;
 	}
 
@@ -38,7 +38,7 @@ void gps_mcudl_linux_unregister_cbs_to_mbraink(void)
 
 	pfn = symbol_get(unregister_gps2mbraink_ops);
 	if (pfn == NULL) {
-		GDL_LOGE("symbol_get fail");
+		GDL_LOGW("symbol_get fail");
 		return;
 	}
 
@@ -50,7 +50,6 @@ void gps_mcudl_linux_unregister_cbs_to_mbraink(void)
 enum gps_mcudl_stat_get_reason gps_mcudl_convert_resaon_from_mbrain(enum mbr2gnss_reason reason)
 {
 	switch (reason) {
-#if 0
 	case MBR2GNSS_TEST:
 		return GPS_MCUDL_STAT_GET_FOR_TEST;
 	case MBR2GNSS_PERIODIC:
@@ -59,10 +58,6 @@ enum gps_mcudl_stat_get_reason gps_mcudl_convert_resaon_from_mbrain(enum mbr2gns
 		return GPS_MCUDL_STAT_GET_MBRAIN_APP_SWITCH;
 	case MBR2GNSS_AP_RESUME:
 		return GPS_MCUDL_STAT_GET_MBRAIN_AP_RESUME;
-#else
-	case TEST:
-		return GPS_MCUDL_STAT_GET_FOR_TEST;
-#endif
 	default:
 		return GPS_MCUDL_STAT_GET_FOR_TEST;
 	}
@@ -72,17 +67,13 @@ enum gnss2mbr_status gps_mcudl_convert_status_to_mbrain(enum gps_mcudl_stat_get_
 {
 	switch (result) {
 	case GPS_MCUDL_STAT_NO_DATA:
-		/* return GNSS2MBR_NO_DATA; */
-		return NO_DATA;
+		return GNSS2MBR_NO_DATA;
 	case GPS_MCUDL_STAT_OK_NO_MORE:
-		/* return GNSS2MBR_OK_NO_MORE; */
-		return DATA_OK_END;
+		return GNSS2MBR_OK_NO_MORE;
 	case GPS_MCUDL_STAT_OK_MORE:
-		/* return GNSS2MBR_OK_MORE; */
-		return DATA_OK_AGAIN;
+		return GNSS2MBR_OK_MORE;
 	default:
-		/* return GNSS2MBR_NO_DATA; */
-		return NO_DATA;
+		return GNSS2MBR_NO_DATA;
 	}
 }
 
@@ -99,28 +90,34 @@ enum gnss2mbr_status gps_mcudl_mbraink2gps_get_lp_data(
 		gps_mcudl_convert_resaon_from_mbrain(reason), &lp_data);
 	ret = gps_mcudl_convert_status_to_mbrain(result);
 
-	/* if ((ret == GNSS2MBR_OK_NO_MORE) || (ret == GNSS2MBR_OK_MORE)) */
-	if ((ret == DATA_OK_END) || (ret == DATA_OK_AGAIN)) {
-		p->hdr.data_size = sizeof(*p);
-		p->hdr.major_ver = LP_DATA_MAJOR_VER;
-		p->hdr.minor_ver = LP_DATA_MINOR_VER;
+	if ((ret == GNSS2MBR_OK_NO_MORE) || (ret == GNSS2MBR_OK_MORE)) {
+		p->hdr_out.data_size = sizeof(*p);
+		p->hdr_out.major_ver = LP_DATA_MAJOR_VER;
+		p->hdr_out.minor_ver = LP_DATA_MINOR_VER;
 
-		/* p->dump_ts    = lp_data.dump_ktime_ms;
-		 * p->dump_index = lp_data.dump_index;
-		 */
-		p->ap_resume_ts     = lp_data.dump_ktime_ms;
-		p->ap_resume_index  = lp_data.dump_index;
+		if (p->hdr_out.data_size <= p->hdr_in.data_size) {
+			p->dump_ts       = lp_data.dump_ktime_ms;
+			p->dump_index    = lp_data.dump_index;
 
-		p->gnss_mcu_sid     = lp_data.gnss_mcu_sid;
-		p->gnss_mcu_is_on   = lp_data.gnss_mcu_is_on;
-		p->gnss_pwr_is_hi   = lp_data.gnss_pwr_is_hi;
-		p->gnss_pwr_wrn     = lp_data.gnss_pwr_wrn;
-		/* p->gnss_pwr_wrn_cnt = lp_data.gnss_pwr_wrn_cnt; */
+			p->gnss_mcu_sid     = lp_data.gnss_mcu_sid;
+			p->gnss_mcu_is_on   = lp_data.gnss_mcu_is_on;
+			p->gnss_pwr_is_hi   = lp_data.gnss_pwr_is_hi;
+			p->gnss_pwr_wrn     = lp_data.gnss_pwr_wrn;
+			p->gnss_pwr_wrn_cnt = lp_data.gnss_pwr_wrn_cnt;
+		} else {
+			GDL_LOGW("ver_sz mismatch: in=%u,%u,%u out=%u,%u,%u",
+				p->hdr_in.major_ver,
+				p->hdr_in.minor_ver,
+				p->hdr_in.data_size,
+				p->hdr_out.major_ver,
+				p->hdr_out.minor_ver,
+				p->hdr_out.data_size
+			);
+		}
 	}
 	return ret;
 #else
-	/* return GNSS2MBR_NO_DATA; */
-	return NO_DATA;
+	return GNSS2MBR_NO_DATA;
 #endif
 }
 
@@ -137,31 +134,41 @@ enum gnss2mbr_status gps_mcudl_mbraink2gps_get_mcu_data(
 		gps_mcudl_convert_resaon_from_mbrain(reason), &mcu_data);
 	ret = gps_mcudl_convert_status_to_mbrain(result);
 
+	if ((ret == GNSS2MBR_OK_NO_MORE) || (ret == GNSS2MBR_OK_MORE)) {
+		p->hdr_out.data_size = sizeof(*p);
+		p->hdr_out.major_ver = MCU_DATA_MAJOR_VER;
+		p->hdr_out.minor_ver = MCU_DATA_MINOR_VER;
 
-	/* if ((ret == GNSS2MBR_OK_NO_MORE) || (ret == GNSS2MBR_OK_MORE)) */
-	if ((ret == DATA_OK_END) || (ret == DATA_OK_AGAIN)) {
-		p->hdr.data_size = sizeof(*p);
-		p->hdr.major_ver = MCU_DATA_MAJOR_VER;
-		p->hdr.minor_ver = MCU_DATA_MINOR_VER;
+		if (p->hdr_out.data_size <= p->hdr_in.data_size) {
+			p->gnss_mcu_sid  = mcu_data.gnss_mcu_sid;
+			p->clock_cfg_val = mcu_data.clock_cfg_val;
 
-		p->gnss_mcu_sid  = mcu_data.gnss_mcu_sid;
-		p->clock_cfg_val = mcu_data.clock_cfg_val;
+			p->open_ts       = mcu_data.open_ktime_ms;
+			p->open_duration = mcu_data.open_duration;
+			p->open_duration_max = mcu_data.open_duration_max;
 
-		p->open_ts       = mcu_data.open_ktime_ms;
-		p->open_duration = mcu_data.open_duration;
-		p->open_duration_max = mcu_data.open_duration_max;
+			p->close_ts      = mcu_data.close_ktime_ms;
+			p->close_duration = mcu_data.close_duration;
+			p->close_duration_max = mcu_data.close_duration_max;
 
-		p->close_ts      = mcu_data.close_ktime_ms;
-		p->close_duration = mcu_data.close_duration;
-		p->close_duration_max = mcu_data.close_duration_max;
-
-		p->has_exception = mcu_data.has_exception;
-		p->exception_cnt = mcu_data.exception_cnt;
+			p->has_exception = mcu_data.has_exception;
+			p->force_close   = mcu_data.force_close;
+			p->exception_cnt = mcu_data.exception_cnt;
+			p->force_close_cnt = mcu_data.force_close_cnt;
+		} else {
+			GDL_LOGW("ver_sz mismatch: in=%u,%u,%u out=%u,%u,%u",
+				p->hdr_in.major_ver,
+				p->hdr_in.minor_ver,
+				p->hdr_in.data_size,
+				p->hdr_out.major_ver,
+				p->hdr_out.minor_ver,
+				p->hdr_out.data_size
+			);
+		}
 	}
 	return ret;
 #else
-	/* return GNSS2MBR_NO_DATA; */
-	return NO_DATA;
+	return GNSS2MBR_NO_DATA;
 #endif
 }
 
