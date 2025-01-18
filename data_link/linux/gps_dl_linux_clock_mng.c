df75324b7427c0758fcdca414c9c16c0e18668f5
Zhenyu Jing <zhenyu.jing@mediatek.com>
2025-01-18 23:45:01 +0800

    [ALPS09430811] GPS: support reading clock inf from 6687
    [Description]
    GPS: support reading clock inf from 6687
    
    [Test]
    Gps works ok.
    
    MTK-Commit-Id: 0dda30b032dae1d0f47fa7c521b05890f6bd59db
    
    CR-Id: ALPS09430811
    Feature: Location Chipset Capability
    Change-Id: I9d04304dff34c711fa1eab46e3a4f6b78021fb9f
    Signed-off-by: Zhenyu Jing <zhenyu.jing@mediatek.com>

--- data_link/linux/gps_dl_linux_clock_mng.c
+++ data_link/linux/gps_dl_linux_clock_mng.c
@@ -19,6 +19,7 @@
 static int gps_dl_clock_mng_register_device(void);
 static int gps_dl_clock_mng_unregister_device(void);
 static int gps_dl_mt6685_probe(struct platform_device *pdev);
+static int gps_dl_mt6687_probe(struct platform_device *pdev);
 
 
 /*******************************************************************************
@@ -41,6 +42,21 @@ static struct platform_driver gps_dl_mt6685_dev_drv = {
 	},
 };
 
+static struct regmap *g_gps_dl_regmap_mt6687;
+static const struct of_device_id gps_dl_clock_mt6687_of_ids[] = {
+	{.compatible = "mediatek,mt6687-gps",},
+	{}
+};
+
+
+static struct platform_driver gps_dl_mt6687_dev_drv = {
+	.probe = gps_dl_mt6687_probe,
+	.driver = {
+		.name = "mt6687-gps",
+		.of_match_table = gps_dl_clock_mt6687_of_ids,
+		.probe_type = PROBE_FORCE_SYNCHRONOUS,
+	},
+};
 
 /*******************************************************************************
 *                              F U N C T I O N S
@@ -54,7 +70,6 @@ int gps_dl_clock_mng_init(void)
 
 int gps_dl_clock_mng_deinit(void)
 {
-	g_gps_dl_regmap_mt6685 = NULL;
 	gps_dl_clock_mng_unregister_device();
 	return 0;
 }
@@ -71,9 +86,38 @@ static int gps_dl_mt6685_probe(struct platform_device *pdev)
 	return 0;
 }
 
+static int gps_dl_mt6687_probe(struct platform_device *pdev)
+{
+	g_gps_dl_regmap_mt6687 = dev_get_regmap(pdev->dev.parent, NULL);
+
+	if (!g_gps_dl_regmap_mt6687) {
+		GDL_LOGE("failed to get g_regmap_mt6687\n");
+		return 0;
+	}
+
+	return 0;
+}
+
 struct regmap *gps_dl_clock_mng_get_regmap(void)
 {
-	return g_gps_dl_regmap_mt6685;
+	if (g_gps_dl_regmap_mt6685 != NULL)
+		return g_gps_dl_regmap_mt6685;
+
+	if (g_gps_dl_regmap_mt6687 != NULL)
+		return g_gps_dl_regmap_mt6687;
+
+	return NULL;
+}
+
+int gps_dl_clock_mng_get_chip_id(void)
+{
+	if (g_gps_dl_regmap_mt6685 != NULL)
+		return 0x6685;
+
+	if (g_gps_dl_regmap_mt6687 != NULL)
+		return 0x6687;
+
+	return 0;
 }
 
 static int gps_dl_clock_mng_register_device(void)
@@ -86,36 +130,59 @@ static int gps_dl_clock_mng_register_device(void)
 	else
 		GDL_LOGI("mt6685 ok.\n");
 
+	ret = platform_driver_register(&gps_dl_mt6687_dev_drv);
+	if (ret)
+		GDL_LOGE("gps clock ic mt6687 driver registered failed(%d)\n", ret);
+	else
+		GDL_LOGI("mt6687 ok.\n");
+
 	return 0;
 }
 
 static int gps_dl_clock_mng_unregister_device(void)
 {
-	platform_driver_unregister(&gps_dl_mt6685_dev_drv);
+	if (g_gps_dl_regmap_mt6685 != NULL) {
+		platform_driver_unregister(&gps_dl_mt6685_dev_drv);
+		g_gps_dl_regmap_mt6685 = NULL;
+	}
+
+	if (g_gps_dl_regmap_mt6687 != NULL) {
+		platform_driver_unregister(&gps_dl_mt6687_dev_drv);
+		g_gps_dl_regmap_mt6687 = NULL;
+	}
+
 	return 0;
 }
-
-#define DCXO_DIGCLK_ELR 0x7f4
+/*6685 clock information*/
+#define MT6685_DCXO_DIGCLK_ELR 0x7f4
+/*6687 clock information*/
+#define MT6687_DCXO_DIGCLK_ELR 0x7ec
 
 int gps_dl_clock_mng_get_platform_clock(void)
 {
 	struct regmap *map = gps_dl_clock_mng_get_regmap();
+	int clock_ic_id = gps_dl_clock_mng_get_chip_id();
 	int value = 0;
 	int retval = 0;
 
 	if (!map) {
 		GDL_LOGE("failed to get regmap.\n");
-		/*from HL req, keep 52M if cannot get freq from clock_ic*/
-		return 1;
+		return -1;
 	}
-
-	regmap_read(map, DCXO_DIGCLK_ELR, &value);
+	if (clock_ic_id == 0x6685)
+		regmap_read(map, MT6685_DCXO_DIGCLK_ELR, &value);
+	else if (clock_ic_id == 0x6687)
+		regmap_read(map, MT6687_DCXO_DIGCLK_ELR, &value);
+	else {
+		GDL_LOGE("neither 6685 or 6687, failed to get right clock_id.\n");
+		return -1;
+	}
 	if (value & 0x1)
 		retval = 1; /*52M*/
 	else
 		retval = 0; /*26M*/
 
-	GDL_LOGW("get clock retval = %d\n", retval);
+	GDL_LOGW("get clock retval = %d, clock_id = 0x%x\n", retval, clock_ic_id);
 	return retval;
 }
 
