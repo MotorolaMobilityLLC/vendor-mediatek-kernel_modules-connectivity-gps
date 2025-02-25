91c5c631bdc4785f05d49efe01f3e28e3d5da15e
Zhenyu Jing <zhenyu.jing@mediatek.com>
2025-02-25 11:24:16 +0800

    [ALPS09775358] gps: Update protection for ATF_ENTRY
    [Description]
    Update protection for ATF_ENTRY.
    
    [Test]
    Gps works ok.
    
    MTK-Commit-Id: 5dd06c3143a58b9dd1fc365c5a8ca6830c0b5f59
    
    CR-Id: ALPS09775358
    Feature: Location Chipset Capability
    Change-Id: Ie42db746deb58e2eeac576871fbff80d214d89e6
    Signed-off-by: Zhenyu Jing <zhenyu.jing@mediatek.com>

--- data_link/hw/gps_dl_hw_atf_power_ctrl.c
+++ data_link/hw/gps_dl_hw_atf_power_ctrl.c
@@ -47,13 +47,13 @@ void gps_dl_hw_may_set_link_power_flag(enum gps_dl_link_id_enum link_id, bool po
 	if (power_ctrl) {
 		if (gps_ap_common_on_flag == 0)
 			arm_smccc_smc(MTK_SIP_KERNEL_GPS_CONTROL, SMC_GPS_COMMON_ON_SET_FLAG_OPID,
-					0xdb9db9, power_ctrl, 0, 0, 0, 0, &res);
+					0xdb9db9, power_ctrl, 0xdb8db8, 0, 0, 0, &res);
 		gps_ap_common_on_flag |= (1UL << link_id);
 	} else {
 		gps_ap_common_on_flag &= ~(1UL << link_id);
 		if (gps_ap_common_on_flag == 0)
 			arm_smccc_smc(MTK_SIP_KERNEL_GPS_CONTROL, SMC_GPS_COMMON_ON_SET_FLAG_OPID,
-					0xdb9db9, power_ctrl, 0, 0, 0, 0, &res);
+					0xdb9db9, power_ctrl, 0xdb8db8, 0, 0, 0, &res);
 	}
 }
 
