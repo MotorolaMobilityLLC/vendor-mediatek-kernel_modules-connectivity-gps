c7594112d3342317087350b30a2fef6a04b0f689
Hua Fu <hua.fu@mediatek.com>
2025-04-28 02:50:14 +0800

    [ALPS09919982] GPS: Add active check in gps_mcudl test functions
    [Description]
    Added checks to ensure GPS MCU is active before executing test
    functions. This prevents operations when the module is inactive.
    
    [Test]
    GNSS can get fixed.
    
    MTK-Commit-Id: ddfce6f978b23b23240b72e9879a4f06031fb19c
    
    Signed-off-by: Hua Fu <hua.fu@mediatek.com>
    CR-Id: ALPS09919982
    Feature: gnss_host_kernel_driver
    Change-Id: I184d8d1c14e165c82372b21c2452ec1f5e88b212
    (cherry-pick from 559f31c7474c4f765debe9d567350475ee80f1da)

--- gps_mcudl/xlink/gps_mcudl_xlink.c
+++ gps_mcudl/xlink/gps_mcudl_xlink.c
@@ -82,6 +82,11 @@ void gps_mcudl_xlink_test_fw_own_ctrl(bool to_set)
 {
 	bool ntf = false;
 
+	if (gps_mcudl_hal_get_open_flag() == 0) {
+		MDL_LOGW("not active, bypass");
+		return;
+	}
+
 	if (to_set) {
 		ntf = gps_mcudl_hal_user_set_fw_own_may_notify(GMDL_FW_OWN_CTRL_BY_TEST);
 		MDL_LOGW("to_set=%d, ntf=%d", to_set, ntf);
@@ -96,6 +101,11 @@ void gps_mcudl_xlink_test_fw_own_ctrl(bool to_set)
 
 void gps_mcudl_xlink_test_toggle_ccif(unsigned int ch)
 {
+	if (gps_mcudl_hal_get_open_flag() == 0) {
+		MDL_LOGW("not active, bypass");
+		return;
+	}
+
 	if (ch >= 8)
 		return;
 	gps_mcudl_hal_ccif_tx_prepare(ch);
@@ -119,6 +129,11 @@ void gps_mcudl_xlink_test_read_mcu_reg(unsigned int addr, unsigned int bytes)
 	bool is_okay;
 	unsigned char buf[12];
 
+	if (gps_mcudl_hal_get_open_flag() == 0) {
+		MDL_LOGW("not active, bypass");
+		return;
+	}
+
 	memset(&buf[0], 0, sizeof(buf));
 	buf[0] = 5;
 	buf[4] = (addr >>  0) & 0xFF;
@@ -133,6 +148,11 @@ void gps_mcudl_xlink_test_query_ver(void)
 {
 	bool is_okay = false;
 
+	if (gps_mcudl_hal_get_open_flag() == 0) {
+		MDL_LOGW("not active, bypass");
+		return;
+	}
+
 	is_okay = gps_mcu_hif_send(GPS_MCU_HIF_CH_DMALESS_MGMT, "\x06", 1);
 	MDL_LOGW("write cmd6, is_ok=%d", is_okay);
 }
@@ -142,6 +162,11 @@ void gps_mcudl_xlink_test_wakeup_ap_later(unsigned int data)
 	bool is_okay = false;
 	unsigned char u8_data;
 	unsigned char send_data[2];
+
+	if (gps_mcudl_hal_get_open_flag() == 0) {
+		MDL_LOGW("not active, bypass");
+		return;
+	}
 
 	u8_data = data & 0xff;
 	send_data[0] = '\x09';
@@ -153,6 +178,11 @@ void gps_mcudl_xlink_test_wakeup_ap_later(unsigned int data)
 void gps_mcudl_xlink_test_send_4byte_mgmt_data(unsigned int data_4byte)
 {
 	bool is_okay = false;
+
+	if (gps_mcudl_hal_get_open_flag() == 0) {
+		MDL_LOGW("not active, bypass");
+		return;
+	}
 
 	is_okay = gps_mcu_hif_send(GPS_MCU_HIF_CH_DMALESS_MGMT,
 		(const unsigned char *)&data_4byte, 4);
