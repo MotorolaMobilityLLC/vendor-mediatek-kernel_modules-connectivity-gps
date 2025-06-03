e09866c4f271c2cde4e7c782b05a2c7f4ab79c6b
Zhenyu Jing <zhenyu.jing@mediatek.com>
2025-06-03 22:01:22 +0800

    [ALPS10067468] gps: update timeout for wakeup read bgf_ip
    [Description]
    gps: update timeout for wakeup read bgf_ip.
    
    [Test]
    Build pass
    
    MTK-Commit-Id: ac31eb72b4f3805f9f2c5322f29017053a6efd85
    
    CR-Id: ALPS10067468
    Feature: Location Chipset Capability
    Signed-off-by: Zhenyu Jing <zhenyu.jing@mediatek.com>
    Change-Id: I95ebd0f2a584a526d57662d568c54fd0e39e37ec

--- data_link/plat/v050/gps_dl_hw_dep_macro.h
+++ data_link/plat/v050/gps_dl_hw_dep_macro.h
@@ -37,7 +37,7 @@
 #define GDL_HW_CHECK_BGF_IP_VER(p_poll_okay, p_poll_ver)             \
 	GDL_HW_POLL_ENTRY_VERBOSE(GPS_DL_GPS_BUS,                 \
 		BGF_GPS_CFG_BGF_IP_VERSION_BGFSYS_VERSION,                    \
-		p_poll_okay, p_poll_ver, POLL_DEFAULT, (                 \
+		p_poll_okay, p_poll_ver, (10 * POLL_DEFAULT2), (                 \
 			(*p_poll_ver == GDL_HW_BGF_VER_MT6893)  ||    \
 			(*p_poll_ver == GDL_HW_BGF_VER_MT6897)  ||    \
 			(*p_poll_ver == GDL_HW_BGF_VER_MT6878)  ||    \
